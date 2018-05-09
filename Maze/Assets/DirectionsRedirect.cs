dba83d9a7324589fa36e80154f0de4174e9367b7
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-05-09 17:40:31 -0500

    Add Directions scene

---
+++ Maze/Assets/DirectionsRedirect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DirectionsRedirect : MonoBehaviour {
+
+	// audio source reference
+	private AudioSource m_audio_src;
+
+	void Start()
+	{
+		m_audio_src = GetComponent<AudioSource>();
+	}
+
+	public void Button_OnClick()
+	{
+		Debug.Log("Directions Clicked"); 
+		//Delete (GameObject.Find ("Canvas"));
+		m_audio_src.Play();
+		SceneManager.LoadScene("Directions", LoadSceneMode.Single);
+
+	}
+}
---
+++ Maze/Assets/Return.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Return : MonoBehaviour {
+
+	// audio source reference
+	private AudioSource m_audio_src;
+
+	void Start()
+	{
+		m_audio_src = GetComponent<AudioSource>();
+	}
+
+	public void Button_OnClick()
+	{
+		Debug.Log("Return Clicked"); 
+		//Delete (GameObject.Find ("Canvas"));
+		m_audio_src.Play();
+		SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+	}
+}
