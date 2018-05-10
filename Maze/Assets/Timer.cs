8d80b1b853dad1f0bb8388ca95dad1d1e4c3fdef
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-05-09 22:39:07 -0500

    Update timer

--- Maze/Assets/Timer.cs
+++ Maze/Assets/Timer.cs
@@ -7,6 +7,7 @@ public class Timer : MonoBehaviour {
 
 	public Text timerText;
 	private float startTime;
+	private float elapsedTime;
 	private bool finished = false;
 	public Text HighScore;
 	public float highScoreState; 
@@ -22,19 +23,8 @@ public class Timer : MonoBehaviour {
 		if (finished)
 			return;
 
-		float t = Time.time - startTime;
-
-		string minutes = ((int)t / 60).ToString("00");
-        string seconds = "";
-        if(t % 60 < 10)
-        {
-             seconds = (t % 60).ToString("f2");
-             string appToMe = "0";
-             seconds = appToMe + seconds;
-        }
-        else { seconds = (t % 60).ToString("f2"); }
-        
-		timerText.text = minutes + ":" + seconds;
+		elapsedTime = Time.time - startTime;	//cache time so it is consistent throughout code
+		timerText.text = getTimeStamp(Time.time - startTime);
 
 		// initializes highScoreState to -1 IF there’s no saved high score. 
 		// IF there is one, then 32 initializes highScoreState to the high score.
@@ -51,28 +41,31 @@ public class Timer : MonoBehaviour {
 		timerText.color = Color.green;
 
 		float highScoreState = PlayerPrefs.GetFloat("HighScore", -1);
-		float newTime = Time.time - startTime;
+		//float newTime = Time.time - startTime;
 
 		if (highScoreState == -1) { 
-			PlayerPrefs.SetFloat("HighScore", newTime); 
+			PlayerPrefs.SetFloat("HighScore", elapsedTime); 
 		} else { 
-			if (newTime < highScoreState) { 
-				PlayerPrefs.SetFloat("HighScore", newTime); 
+			if (elapsedTime < highScoreState) { 
+				PlayerPrefs.SetFloat("HighScore", elapsedTime); 
 			} 
 		}
 
 		highScoreState = PlayerPrefs.GetFloat("HighScore", -1);
 
-//		string minutes = ((int)highScoreState / 60).ToString();
-//		string seconds = (highScoreState % 60).ToString("f2");
-
-		//HighScore.text = PlayerPrefs.GetFloat("HighScore", 999).ToString();
 		HighScore.text = getTimeStamp(PlayerPrefs.GetFloat("HighScore", 999)); 
 	}
 
 	public string getTimeStamp(float time) {
-		string minutes = ((int)highScoreState / 60).ToString("00");
-		string seconds = (highScoreState % 60).ToString("00");
+		string minutes = ((int)time / 60).ToString("00");
+		string seconds = "";
+		if(time % 60 < 10)
+		{
+			seconds = (time % 60).ToString("f2");
+			string appToMe = "0";
+			seconds = appToMe + seconds;
+		}
+		else { seconds = (time % 60).ToString("f2"); }
 
 		return minutes + ":" + seconds;
 	}
