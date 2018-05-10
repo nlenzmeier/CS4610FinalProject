9cb64177e4536408876aa8e5f9e60c205921cafe
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-05-09 23:22:07 -0500

    Add quit button

---
+++ Maze/Assets/Quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour {
+
+	private AudioSource m_audio_src;
+
+	// Use this for initialization
+	void Start () {
+		m_audio_src = GetComponent<AudioSource>();
+	}
+
+	public void Button_OnClick()
+	{
+		Debug.Log("Quit Clicked");
+		Application.Quit();
+	}
+}
